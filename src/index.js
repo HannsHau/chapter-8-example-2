f00639951db5d2ad7d0738e448b946b785af7297
HannsHau <bierkoenig@web.de>
2025-07-18 20:26:34 +0200

    lets go

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.get("/live", (req, res) => {
 //
 // Registers a HTTP GET route for video streaming.
 //
-app.get("/video", async (req, res) => { // Route for streaming video.
+app.get("/video", async (req, res) => { // Route for streaming video..
     
     const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
     const stats = await fs.promises.stat(videoPath);
